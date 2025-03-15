f861e6c6d1322dd0ed9deae975df9d742eedc85f
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-03-15 01:58:40 +0100

    Strapi themplate for ec2 server

---
+++ dist/src/api/conductor/services/conductor.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * conductor service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::conductor.conductor');
